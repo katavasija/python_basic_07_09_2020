0616e8ffc0f57c35df3642701ec3eba4e434d922
Romanenko Konstantin <romanenkok81@gmail.com>
2020-09-21 23:26:24 +0700

    task7

---
+++ les4/task7.py
@@ -0,0 +1,30 @@
+"""
+Реализовать генератор с помощью функции с ключевым словом yield, создающим очередное значение.
+При вызове функции должен создаваться объект-генератор.
+Функция должна вызываться следующим образом: for el in fact(n).
+Функция отвечает за получение факториала числа,
+а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
+"""
+
+
+def fact(n):
+    if n < 0:
+        return None
+    elif n == 0:
+        return 1
+    else:
+        factorial = 1
+        for f in range(1, n + 1):
+            factorial *= f
+            yield factorial
+
+
+if __name__ == "__main__":
+    while True:
+        n_str = input("Введите n > 0\n")
+        if n_str.isdigit():
+            if int(n_str) > 0:
+                break
+    n = int(n_str)
+    for el in fact(n):
+        print(el)
